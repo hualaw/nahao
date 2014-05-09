de6f4ab684c1abca5cc199dc10dde5fc4eb86749
yanrui <yanrui@91waijiao.com>
2014-05-09 11:14:58 +0800

    一个c-b-m调用的例子

--- application/controllers/www/welcome.php
+++ application/controllers/www/welcome.php
@@ -8,9 +8,13 @@ class Welcome extends NH_Student_Controller {
 
 	public function index()
 	{
-        $this->load->database();
-        $arr = $this->db->query('select * from admin')->result_array();
-        var_dump($arr);exit;
+//        $this->load->database();
+//        $arr = $this->db->query('select * from admin')->result_array();
+//        var_dump($arr);exit;
+
+        $this->load->model('business/student/Business_Student','student');
+        $arr_return = $this->student->get_student();
+        var_dump($arr_return);exit;
 		echo 'This is index of nahaodev student index !';exit;
 		$this->load->view('welcome_message');
 	}
---
+++ application/models/business/student/business_student.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * studnet相关逻辑
+ * Class Model_Student
+ * @author yanrui@91waijiao.com
+ */
+class Business_Student extends NH_Model{
+
+    public function get_student(){
+        $arr_result = array();
+        $this->load->model('model/student/model_student','model_student');
+        $arr_result = $this->model_student->get_student();
+        return $arr_result;
+    }
+}
