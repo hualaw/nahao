e0f3e39c3ec46f3abc1fb488a2f5723401214ee4
yanrui <yanrui@91waijiao.com>
2014-05-14 10:03:13 +0800

    整理结构

---
+++ application/views/admin/header.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<link rel="shortcut icon" href="">
+<title>那好管理系统</title>
+<link href="<?php echo static_url("/admin/css/bootstrap.css"); ?>" rel="stylesheet">
+<link href="<?php echo static_url("/admin/css/nav.css"); ?>" rel="stylesheet">
--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -1,26 +1,14 @@
 <!DOCTYPE html>
 <html lang="zh-cn">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-        <link rel="shortcut icon" href="">
-        <title>那好管理系统</title>
-        <link href="<?php echo static_url("/admin/css/bootstrap.css"); ?>" rel="stylesheet">
-        <link href="<?php echo static_url("/admin/css/nav.css"); ?>" rel="stylesheet">
+        <?php include(APPPATH . 'views/admin/header.php') ?>
     </head>
 
     <body>
-
         <div class="container">
             <?php include(APPPATH . 'views/admin/nav.php') ?>
             <?php echo $content_for_layout; ?>
         </div>
-
-        <!-- Bootstrap core JavaScript
-            ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
-        <script src="<?php echo static_url("/admin/js/bootstrap.min.js"); ?>"></script>
+        <?php include(APPPATH . 'views/admin/footer.php') ?>
     </body>
 </html>
