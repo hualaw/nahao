19a5609ada8846c232e8792efa92fdebd9fce26b
chengchunjuan <chengchunjuan@tizi.com>
2014-05-28 16:14:04 +0800

    教室 评价，反馈，统计，选择练习题4个页面; 选择练习题：左右点击滚动，点击选中和取消当前题目，点击图标快速切换题目；购买前选开课时间只能选一个

--- static/debug/module/classRoom/init.js
+++ static/debug/module/classRoom/init.js
@@ -2,4 +2,6 @@ define(function(require,exports){
 	var _classRoom = require("module/classRoom/classRoom");
 	//做题选答案 (加背景色)
 	_classRoom.options();
+	//选择练习题  左右点击切换 题目选中
+	_classRoom.itemClick();
 })
