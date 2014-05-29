b9ec86a15bb6cd0eab389f26b5ae8aa5250f755d
yanrui <yanrui@91waijiao.com>
2014-05-29 10:39:21 +0800

    提交一个根据id取subject 的共用方法

--- application/models/business/common/business_course_type.php
+++ application/models/business/common/business_course_type.php
@@ -23,4 +23,31 @@ class Business_Course_Type extends NH_Model {
         $arr_return = $this->model_course_type->get_course_type_by_param($str_table_range, $str_result_type, $str_fields);
         return $arr_return;
     }
+
+    /**
+     * 拿到id为键name为值的course_types数组
+     * @return array
+     * @author yanrui@tizi.com
+     */
+    public function get_course_types_like_kv(){
+        $arr_course_types = self::get_course_types();
+        $arr_return = self::converse_array_to_kv($arr_course_types);
+        return $arr_return;
+    }
+
+    /**
+     * 把普通数组转换为id为键name为值的数组
+     * @param $arr_param
+     * @return array
+     * @author yanrui@tizi.com
+     */
+    public function converse_array_to_kv($arr_param){
+        $arr_return = array();
+        if(is_array($arr_param) AND $arr_param){
+            foreach($arr_param as $k => $v){
+                $arr_return[$v['id']] = $v['name'];
+            }
+        }
+        return $arr_return;
+    }
 }
