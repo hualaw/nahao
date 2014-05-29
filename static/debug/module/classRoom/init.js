89be1504181c7418c9907826abe70a1770ae20b5
chengchunjuan <chengchunjuan@tizi.com>
2014-05-29 17:58:54 +0800

    那好 学生端 所有的验证

--- static/debug/module/classRoom/init.js
+++ static/debug/module/classRoom/init.js
@@ -4,4 +4,10 @@ define(function(require,exports){
 	_classRoom.options();
 	//选择练习题  左右点击切换 题目选中
 	_classRoom.itemClick();
+
+	var _valid = require("module/classRoom/valid");
+    // 教室-意见反馈 验证
+    _valid.feedbackForm();
+    // 教室-评价 验证
+    _valid.evaluForm();
 })
