ff70db25619e1a596c1715966c8e8978aa0d2ada
lichuang <lichuang@91waijiao.com>
2014-05-29 17:26:38 +0800

    添加弹出脚本

--- static/debug/public/config.js
+++ static/debug/public/config.js
@@ -62,6 +62,7 @@ seajs.use('jquery',function(){
 		// 模块是课酬结算
 		case 'teacherPay':
 		seajs.use("module/teacherPay/init");
+        break;
 		// 模块是教室
 		case 'classRoom':
 		seajs.use("module/classRoom/init");
