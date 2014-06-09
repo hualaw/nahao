abb3ca830d9a03e2b172d1ca71012c3135fd9826
chengchunjuan <chengchunjuan@tizi.com>
2014-06-09 16:31:03 +0800

    表单 name

--- static/debug/public/config.js
+++ static/debug/public/config.js
@@ -21,7 +21,6 @@ seajs.use('jquery', function () {
     seajs.use('module/nahaoCommon/init');
     // 得到当前页面是哪个
     var _page = $('#nahaoModule').attr('module');
-<<<<<<< HEAD
 
     seajs.use("module/"+_page+"/init");
 //    switch (_page) {
@@ -104,9 +103,8 @@ seajs.use('jquery', function () {
 //            break;
 //    }
 
-=======
+
     seajs.use("module/"+_page+"/init");
->>>>>>> 9531935e1b63f78903868e9213dc3174ba7425a4
     switch (_page) {
         // 模块是学生首页
         case 'studentHomePage':
@@ -187,8 +185,4 @@ seajs.use('jquery', function () {
             seajs.use("module/adminQuestion/init");
             break;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 9531935e1b63f78903868e9213dc3174ba7425a4
 })
