6b056fc028f596783919ea568b0ad8ddc68dad67
chengchunjuan <chengchunjuan@tizi.com>
2014-06-10 17:20:30 +0800

    教室 做题

--- static/debug/module/studentStartClass/init.js
+++ static/debug/module/studentStartClass/init.js
@@ -1,7 +1,4 @@
 define(function(require,exports){
-	// 选择学校组件
-	require('module/common/method/setSchool');
-	
 	if ($(".popBox").length>0) {
 		require("module/common/method/popUp").popUp(".popBox");
 	};
