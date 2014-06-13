d103b050f6db09293873998d818eca0a656f8b14
liuhua <liuhua@91waijiao.com>
2014-06-13 11:19:58 +0800

    增加online配置

--- application/config/production/redis.php
+++ application/config/production/redis.php
@@ -2,20 +2,22 @@
 
 //default redis for auto login and verify with ci
 $config['redis_db']['session']		= 1;//SESSION
+$config['redis_db']['login']	 	= 2;//store phone captcha and email verify code
+$config['redis_db']['student_answer'] = 3; //学生答题记录与统计
 
 
 // Default connection group
-$config['redis_default']['host'] = '192.168.11.75';     // IP address or host
+$config['redis_default']['host'] = '127.0.0.1';     // IP address or host
 $config['redis_default']['port'] = '6379';      // Default Redis port is 6379
 $config['redis_default']['password'] = '';      // Can be left empty when the server does not require AUTH
 $config['redis_default']['timeout'] = 0.25;
 
-$config['redis_slave']['host'] = '192.168.11.75';
+$config['redis_slave']['host'] = '127.0.0.1';
 $config['redis_slave']['port'] = '6379';
 $config['redis_slave']['password'] = '';       // Can be left empty when the server does not require AUTH
 $config['redis_slave']['timeout'] = 0.25;
 
-$config['redis_backup']['host'] = '192.168.11.75';
+$config['redis_backup']['host'] = '127.0.0.1';
 $config['redis_backup']['port'] = '6379';
 $config['redis_backup']['password'] = '';
 $config['redis_backup']['timeout'] = 0.25;
