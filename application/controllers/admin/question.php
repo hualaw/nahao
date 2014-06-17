c1560359fee48889cc042334f18ac7c53245b1fc
songweijun <songweijun@tizi.com>
2014-06-17 16:48:43 +0800

    课题目管理，同课节。带上传图片

--- application/controllers/admin/question.php
+++ application/controllers/admin/question.php
@@ -15,6 +15,8 @@ class Question extends NH_Admin_Controller {
         parent::__construct();
         $this->load->model('business/admin/business_lesson');
         $this->load->model('business/admin/business_question');
+        $this->load->model('business/admin/business_class');
+        header('Content-Type:text/html;CHARSET=utf-8');
     }
 
     /**
@@ -43,7 +45,6 @@ class Question extends NH_Admin_Controller {
      * 课节提交添题
      */
     public function lesson_add(){
-    	header('Content-Type:text/html;CHARSET=utf-8');
     	$param['lesson_id'] = $this->input->post('lesson_id');
     	if(!$param['lesson_id']){
     		echo '<script>alert("缺少必要参数");window.history.go(-1);</script>';
@@ -98,7 +99,6 @@ class Question extends NH_Admin_Controller {
      * 课节提交改题
      */
     public function lesson_doEdit(){
-    	header('Content-Type:text/html;CHARSET=utf-8');
     	$param['lesson_id'] = $this->input->post('lesson_id');
     	$param['question_id'] = $this->input->post('question_id');
     	if(!$param['lesson_id'] || !$param['question_id']){
@@ -149,25 +149,124 @@ class Question extends NH_Admin_Controller {
      */
     public function class_question(){
     	#1. 课信息
-    	$class_id = $this->uri->segment(3,0);
-    	
+        $class_id = $this->uri->segment(3,0);
+        $class_info = $this->business_class->get_class_by_id($class_id);
+        #2. 课题目列表
+        $question_list = $this->business_question->class_question(array('class_id'=>$class_id));
+//        var_dump($class_info);var_dump($question_list);die;
+        $_GET['tab'] = "question_add";
+		$data = array(
+			'class_info' => $class_info,
+			'question_list' => $question_list,
+			'total' => count($question_list),
+			'class_id' => $class_id,
+			'tab' => in_array($_GET['tab'],array('question_list','question_add','question_edit')) ? $_GET['tab'] : 'question_list' ,
+		);
+    	$this->smarty->assign('view', 'question_class_list');
+    	$this->smarty->assign('data', $data);
+        $this->smarty->display('admin/layout.html');
     }
     
     /**
      * 课添题
      */
-    public function class_question_add(){
-    	#1. 课信息
-    	$class_id = $this->uri->segment(3,0);
+    public function class_add(){
+    	$param['class_id'] = $this->input->post('class_id');
+    	if(empty($param['class_id'])){
+    		echo '<script>alert("缺少必要参数");window.history.go(-1);</script>';
+    	}
+    	$param['question'] = $this->input->post('question');//'Aaaaaaa'
+    	$param['analysis'] = $this->input->post('analysis');//'Aaaaaaa'
+    	$param['answer'] = $this->input->post('answer');//'A'
+    	$param['A'] = $this->input->post('A');//'Aaaaaaa'
+    	$param['B'] = $this->input->post('B');
+    	$param['C'] = $this->input->post('C');
+    	$param['D'] = $this->input->post('D');
+    	$param['E'] = $this->input->post('E');
+    	$param['do'] = 'add';
+    	$param['add_class_question'] = 1;
+    	$res = $this->business_question->class_question_doWrite($param);
     	
+    	if($res){
+			echo '<script>alert("添加成功");if(confirm("是否继续添加？")){window.location.href="/question/class_question/'.$param['class_id'].'/?tag=question_add&tmp='.time().'";}else{window.location.href="/question/class_question/'.$param['class_id'].'/?tmp='.time().'"};</script>';
+		}else{
+			echo '<script>alert("添加失败");window.history.go(-1);</script>';
+		}
+		exit;
+    }
+    
+    /**
+     * 修改界面
+     */
+    public function class_edit(){
+    	#1. 课信息
+        $class_id = $this->uri->segment(3,0);
+        $question_id = $this->uri->segment(4,0);
+        $class_info = $this->business_class->get_class_by_id($class_id);
+        #2. 课题目列表
+        $param = array(
+        	'class_id'=>$class_id,
+        	'question_id'=>$question_id,
+        );
+        $question_list = $this->business_question->class_question($param);
+        if(!isset($question_list[0])){exit('题目不存在或内容不全');}
+        $data = array(
+        	'class_id' => $class_id,
+        	'question_id' => $question_id,
+			'class_info' => $class_info,
+			'question_info' => isset($question_list[0]) ? $question_list[0] : array(),
+		);
+		$this->smarty->assign('view', 'question_class_edit');
+    	$this->smarty->assign('data', $data);
+        $this->smarty->display('admin/layout.html');
+    }
+    
+    /**
+     * 课提交改题
+     */
+    public function class_doEdit(){
+    	$param['class_id'] = $this->input->post('class_id');
+    	$param['question_id'] = $this->input->post('question_id');
+    	if(!$param['class_id'] || !$param['question_id']){
+    		echo '<script>alert("缺少必要参数");window.history.go(-1);</script>';
+    	}
+    	$param['question'] = $this->input->post('question');//'Aaaaaaa'
+    	$param['analysis'] = $this->input->post('analysis');//'Aaaaaaa'
+    	$param['answer'] = $this->input->post('answer');//'A'
+    	$param['A'] = $this->input->post('A');//'Aaaaaaa'
+    	$param['B'] = $this->input->post('B');
+    	$param['C'] = $this->input->post('C');
+    	$param['D'] = $this->input->post('D');
+    	$param['E'] = $this->input->post('E');
+    	$param['do'] = 'edit';//这个别忘了
+    	$res = $this->business_question->class_question_doWrite($param);
+    	
+    	if($res){
+			echo '<script>alert("修改成功");window.location.href="/question/class_question/'.$param['class_id'].'/?tmp='.time().'";</script>';
+		}else{
+			echo '<script>alert("修改失败");window.history.go(-1);</script>';
+		}
     }
     
     /**
      * 课删题
      */
     public function class_question_delete(){
-    	#1. 课信息
+    	#1. 信息
     	$class_id = $this->uri->segment(3,0);
-    	
+    	$question_id = $this->uri->segment(4,0);
+    	#2. 执行删除,只删关系
+    	$param = array(
+    		'class_id' => $class_id,
+    		'question_id' => $question_id,
+    		'do' => 'delete',
+    		'delete_class_question' => 1,
+    	);
+    	$res = $this->business_question->class_question_delete($param);
+    	if($res){
+    		echo '<script>alert("删除成功");window.location.href="/question/class_question/'.$class_id.'/?tmp='.time().'"</script>';
+    	}else{
+    		echo '<script>alert("删除失败");history.go(-1);</script>';
+    	}
     }
-}    
+}
