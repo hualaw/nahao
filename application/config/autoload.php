3b1ff1f4ca78eef1e860af429ea68dc46c434a28
liuhua <liuhua@tizi.com>
2014-06-17 17:59:51 +0800

    去掉自动载入url helper，因为nh_url已经都有了

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session', 'smarty');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('language', 'cookie', 'url', 'public','encrypt','phone','string', 'nh_url');
+$autoload['helper'] = array('language', 'cookie', 'public','encrypt','phone','string', 'nh_url');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Config files
