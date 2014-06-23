01b4c81e0d367b454510866b10ffee03e7e89355
yanrui <yanrui@tizi.com>
2014-06-23 18:55:16 +0800

    admin login DB_init edit

--- application/core/NH_Admin_Controller.php
+++ application/core/NH_Admin_Controller.php
@@ -30,7 +30,8 @@ class NH_Admin_Controller extends NH_Controller
 //            o($bool_login_flag);
             if($this->is_login===true){
                 //验证登录通过后拿到userinfo
-                $this->load->vars('userinfo',$this->session->all_userdata());
+		$this->userinfo = $this->session->all_userdata();
+                $this->load->vars('userinfo',$this->userinfo);
                 $this->smarty->assign('userinfo',$this->userinfo);
                 $this->smarty->assign('current',$this->current);
                 $this->smarty->assign('js_module', DOMAIN.ucfirst($this->current['controller']));
