418c795fad7c3e536c2779620c0e62ae697e5e55
shangshikai <shangshikai@tizi.com>
2014-06-24 15:51:14 +0800

    公告管理 评价管理

--- application/controllers/admin/affiche.php
+++ application/controllers/admin/affiche.php
@@ -7,13 +7,17 @@
          */
         public function index()
         {
+            $round_id=$this->input->get('round_id',TRUE) ? $this->input->get('round_id',TRUE) : 0;
+            //echo $round_id;die;
+            $round_title=$this->input->get('round_title',TRUE) ? $this->input->get('round_title',TRUE) : '请输入公告内容';
+           // echo $round_title;die;
             $admin_name=$this->input->get('admin_name',TRUE) ? $this->input->get('admin_name',TRUE) : "";
             $author_role=$this->input->get('author_role',TRUE) ? $this->input->get('author_role',TRUE) : 0;
             $content=$this->input->get('content',TRUE) ? $this->input->get('content',TRUE) : "";
             $start_time=$this->input->get('start_time',TRUE) ? $this->input->get('start_time',TRUE) : "";
             $end_time=$this->input->get('end_time',TRUE) ? $this->input->get('end_time',TRUE) : "";
             $status=$this->input->get('status',TRUE) ? $this->input->get('status',TRUE) : 0;
-            $total=$this->affiche->total_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status);
+            $total=$this->affiche->total_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status,$round_id);
             $search_total=$total;
             $this->load->library('pagination');
             $config = config_item('page_admin');
@@ -25,16 +29,72 @@
             $int_start=$this->uri->segment(3);
             $this->db->limit(PER_PAGE_NO,$int_start);
             $page = $this->pagination->create_links();
-            $list_affiche=$this->affiche->list_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status);
+            $list_affiche=$this->affiche->list_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status,$round_id);
 
             $config_role=config_item('author_role');
             $affiche_status=config_item('affiche_status');
             $this->smarty->assign('affiche_status',$affiche_status);
+            $this->smarty->assign('round_title',$round_title);
+            $this->smarty->assign('round_id',$round_id);
             $this->smarty->assign('config_role',$config_role);
             $this->smarty->assign('page',$page);
             $this->smarty->assign('list_affiche',$list_affiche);
             $this->smarty->assign('search_total',$search_total);
             $this->smarty->assign('view','affiche');
             $this->smarty->display('admin/layout.html');
+        }
+        /**
+         * 编辑公告内容
+         * @author shangshikai@tizi.com
+         */
+        public function edit_content()
+        {
+            $post=$this->input->post(NULL,TRUE);
+            echo $this->affiche->content_edit($post);
+        }
+        /**
+         * 公告通过审核
+         * @author shangshikai@tizi.com
+         */
+        public function pass()
+        {
+            $id=$this->input->post('id',TRUE);
+            echo $this->affiche->affiche_pass($id);
+        }
+        /**
+         * 公告不通过审核
+         * @author shangshikai@tizi.com
+         */
+        public function nopass()
+        {
+            $id=$this->input->post('id',TRUE);
+            echo $this->affiche->affiche_nopass($id);
+        }
+        /**
+         * 公告置顶
+         * @author shangshikai@tizi.com
+         */
+        public function top()
+        {
+            $id=$this->input->post('id',TRUE);
+            echo $this->affiche->affiche_top($id);
+        }
+        /**
+         * 公告取消置顶
+         * @author shangshikai@tizi.com
+         */
+        public function notop()
+        {
+            $id=$this->input->post('id',TRUE);
+            echo $this->affiche->affiche_notop($id);
+        }
+        /**
+         * 添加公告
+         * @author shangshikai@tizi.com
+         */
+        public function insert_affiche()
+        {
+            $post=$this->input->post(NULL,TRUE);
+            echo $this->affiche->affiche_insert($post);
         }
     }
--- application/models/business/admin/business_affiche.php
+++ application/models/business/admin/business_affiche.php
@@ -11,24 +11,100 @@
          * 公告展示
          * @author shangshikai@tizi.com
          */
-        public function list_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status)
+        public function list_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status,$round_id)
         {
             $admin_name=trim($admin_name);
             $content=trim($content);
             $start_time=strtotime($start_time);
             $end_time=strtotime($end_time);
-            return $this->model_affiche->affiche_list($admin_name,$author_role,$content,$start_time,$end_time,$status);
+            return $this->model_affiche->affiche_list($admin_name,$author_role,$content,$start_time,$end_time,$status,$round_id);
         }
         /**
          * 公告数量
          * @author shangshikai@tizi.com
          */
-        public function total_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status)
+        public function total_affiche($admin_name,$author_role,$content,$start_time,$end_time,$status,$round_id)
         {
             $admin_name=trim($admin_name);
             $content=trim($content);
             $start_time=strtotime($start_time);
             $end_time=strtotime($end_time);
-            return $this->model_affiche->affiche_total($admin_name,$author_role,$content,$start_time,$end_time,$status);
+            return $this->model_affiche->affiche_total($admin_name,$author_role,$content,$start_time,$end_time,$status,$round_id);
+        }
+        /**
+         * 公告编辑
+         * @author shangshikai@tizi.com
+         */
+        public function content_edit($post)
+        {
+            $post['content']=trim(htmlspecialchars($post['content']));
+            return $this->model_affiche->modify_content($post);
+        }
+        /**
+         * 公告通过审核
+         * @author shangshikai@tizi.com
+         */
+        public function affiche_pass($id)
+        {
+            return $this->model_affiche->pass_affiche($id);
+        }
+        /**
+         * 公告不通过审核
+         * @author shangshikai@tizi.com
+         */
+        public function affiche_nopass($id)
+        {
+            return $this->model_affiche->nopass_affiche($id);
+        }
+        /**
+         * 公告置顶
+         * @author shangshikai@tizi.com
+         */
+        public function affiche_top($id)
+        {
+            return $this->model_affiche->top_affiche($id);
+        }
+        /**
+         * 公告取消置顶
+         * @author shangshikai@tizi.com
+         */
+        public function affiche_notop($id)
+        {
+            return $this->model_affiche->notop_affiche($id);
+        }
+        /**
+         * 添加公告
+         * @author shangshikai@tizi.com
+         */
+        public function affiche_insert($post)
+        {
+            if($post['role']==1)//1是管理员发的 3是老师发的
+            {
+                //管理员发公告
+                $admin_id=$this->userinfo['id'];
+                $data=array(
+                    'round_id'=>$post['round_id'],
+                    'author'=>$admin_id,
+                    'author_role'=>1,
+                    'content'=>$post['content'],
+                    'create_time'=>time(),
+                    'status'=>1,
+                    'top_time'=>0
+                );
+            }
+            if($post['role']==3)
+            {
+                //老师发公告,目前还没有此功能
+                $data=array(
+                    'round_id'=>$post['round_id'],
+                    'author'=>$post['teacher_id'],
+                    'author_role'=>3,
+                    'content'=>$post['content'],
+                    'create_time'=>time(),
+                    'status'=>1,
+                    'top_time'=>0
+                );
+            }
+            return $this->model_affiche->add_affiche($data);
         }
     }
