f59c6f08d86f61c3a0c99b3b402fdcd3020643e1
windguo <guoyanfeng@tiiz.com>
2014-06-27 17:05:28 +0800

    更新选择学校和解决tab。js的冲突

--- static/debug/module/login/tab.js
+++ static/debug/module/login/tab.js
@@ -5,12 +5,9 @@ define(function(require,exports){
     		var _index=$(this).index();
     		$(this).addClass("active").siblings("a").removeClass("active");
     		$(".contentBox .box").eq(_index).show().siblings(".box").hide();
-<<<<<<< HEAD
     		// 请求focus的时候出现提示文字的样式
         	seajs.use("module/login/validFocus");
-=======
     		$(".box:eq("+_index+") form input").eq(0).focus();
->>>>>>> 444ccd44ddb6fb54ce4c0c160793e9f1db8acf7c
     	});
     }
 })
