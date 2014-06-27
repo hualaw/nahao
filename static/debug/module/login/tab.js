ab294a7032ee34ac6c8cd8dac5b2e95582928ef4
windguo <guoyanfeng@tiiz.com>
2014-06-27 11:54:05 +0800

    更改注册的提示请求js

--- static/debug/module/login/tab.js
+++ static/debug/module/login/tab.js
@@ -4,7 +4,9 @@ define(function(require,exports){
     	$(".tabBox a").click(function(){
     		var _index=$(this).index();
     		$(this).addClass("active").siblings("a").removeClass("active");
-    		$(".contentBox .box").eq(_index).show().siblings(".box").hide()
+    		$(".contentBox .box").eq(_index).show().siblings(".box").hide();
+    		// 请求focus的时候出现提示文字的样式
+        	seajs.use("module/login/validFocus");
     	});
     }
 })
