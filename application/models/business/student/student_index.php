4407a0a6e5a4c3cf6edd99ca826ad9b4360ee855
songweijun <songweijun@tizi.com>
2014-06-27 12:14:12 +0800

    update

--- application/models/business/student/student_index.php
+++ application/models/business/student/student_index.php
@@ -94,9 +94,11 @@ class Student_Index extends NH_Model{
 	 **/
     public function save_apply_teach($param)
     {
+//    	var_dump($param);die;
     	foreach ($param as $key => $val){
     		if(!$val && !in_array($key,array('schoolname','school','city'))){
-    			exit('<script>alert("'.$key.'不能为空，请重新填写");history.go(-1);</script>');
+    			echo json_encode(array('status'=>'error','msg'=>$key.'不能为空，请重新填写'));die;
+//    			exit('<script>alert("'.$key.'不能为空，请重新填写");history.go(-1);</script>');
     		}
     	}
     	return $this->model_index->save_apply_teach($param);
