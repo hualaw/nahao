69f032dd63631bd1f960b86916781808635f9f55
chengchunjuan <chengchunjuan@tizi.com>
2014-07-03 18:29:56 +0800

    手机发送验证码 倒计时

--- static/debug/module/common/method/countDown.js
+++ static/debug/module/common/method/countDown.js
@@ -1,21 +1,17 @@
 define(function(require,exports){
 	exports.countDown = function (_this){
-        var oswitch = true;
 		//验证倒计时
-        if(oswitch){
-            var ind=60;
-            var timer = setInterval(function(){
-                ind--;
-                if(ind<0){
-                    clearInterval(timer);
-                    _this.show(); 
-                    _this.next('span.codeSpan').hide();
-                }else{
-                    _this.hide(); 
-                    _this.next('span.codeSpan').show().html(ind + '秒后获取验证码');
-                }
-            }, 1000);
-            oswitch = false;
-        }
+        var ind=60;
+        var timer = setInterval(function(){
+            ind--;
+            if(ind<0){
+                clearInterval(timer);
+                _this.show(); 
+                _this.next('span.codeSpan').hide();
+            }else{
+                _this.hide(); 
+                _this.next('span.codeSpan').show().html(ind + '秒后获取验证码');
+            }
+        }, 1000);
 	}
 })
