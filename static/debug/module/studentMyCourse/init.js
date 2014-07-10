795c3e43a72cdeae1ade1370f1ab7fe33c94586f
songweijun <songweijun@tizi.com>
2014-07-10 18:11:25 +0800

    update

--- static/debug/module/studentMyCourse/init.js
+++ static/debug/module/studentMyCourse/init.js
@@ -79,18 +79,21 @@ define(function(require,exports){
     	$('.videoPlay').children().children('.container').each(function(){
 			var noflash = '';
 			var curfile = $(this).attr('title');
+			var curimg = $(this).children('img').attr('src');
+			if(!(curimg.length>0)){
+				curimg = "http://tizi.oss.aliyuncs.com/static/zhuangyuan/index_video.jpg";
+			}
 	    	/*初始化视频播放开始*/
 		    var playlist = [{
 			    "domain": '',
 			    "file": curfile,
-			    "image": $(this).children('img').attr('src')
-//			    "image": "http://tizi.oss.aliyuncs.com/static/zhuangyuan/index_video.jpg"
+			    "image": curimg
 			}];
 		    TiZiplayer($(this).attr('id')).setup({
 				playlist:playlist,
 				primary:"flash",
 				height:405,
-				width:480
+				width:600
 				// ,
 				// adCover:"http://tizi.oss.aliyuncs.com/static/zhuangyuan/video.jpg",
 				// adEnd:"http://tizi.oss.aliyuncs.com/static/zhuangyuan/video.jpg"
