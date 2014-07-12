cc5d0560262b83cb0d8a0ae0d1dbb464fc1dccc8
shangshikai <shangshikai@tizi.com>
2014-07-12 10:57:00 +0800

    修改统计时段上课数

--- application/models/model/admin/model_index.php
+++ application/models/model/admin/model_index.php
@@ -6,9 +6,11 @@
          */
         public function num_goclass($str_start_time,$str_end_time)
         {
-            $this->db->select(TABLE_ROUND.'.id,title,caps')->from(TABLE_ROUND);
-            $this->db->where(TABLE_ROUND.'.start_time<=',$str_start_time);
-            $this->db->where(TABLE_ROUND.'.end_time>=',$str_end_time);
+            $this->db->select(TABLE_CLASS.'.id,'.TABLE_CLASS.'.title,'.TABLE_CLASS.'.round_id,'.TABLE_ROUND.'.caps,'.TABLE_ROUND.'.title as round_title')->from(TABLE_CLASS)->join(TABLE_ROUND,TABLE_ROUND.'.id='.TABLE_CLASS.'.round_id','left');
+            $this->db->where(TABLE_CLASS.'.begin_time<=',$str_start_time);
+            $this->db->where(TABLE_CLASS.'.end_time>=',$str_end_time);
+            $this->db->where(TABLE_CLASS.'.status!=',5);
+            $this->db->where(TABLE_CLASS.'.status!=',6);
             return $this->db->get()->result_array();
         }
     }
