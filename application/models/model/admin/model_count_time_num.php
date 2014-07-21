5d689473478688675f48fc3627d9bf33d360108c
shangshikai <shangshikai@tizi.com>
2014-07-21 12:10:56 +0800

    显示结课的课程 屏蔽下架的课程

--- application/models/model/admin/model_count_time_num.php
+++ application/models/model/admin/model_count_time_num.php
@@ -6,17 +6,17 @@
          */
         public function num_goclass($str_start_time,$str_end_time)
         {
-            $sql="SELECT class.id, class.title, class.round_id, round.caps, round.title as round_title, class.begin_time, class.end_time, class.classroom_id
+            $sql="SELECT class.id, class.status, class.title, class.round_id, round.caps, round.title as round_title, class.begin_time, class.end_time, class.classroom_id
     FROM class
     LEFT JOIN round ON round.id=class.round_id
     WHERE (class.begin_time<= $str_start_time OR class.begin_time<$str_end_time)
     AND (class.end_time>= $str_end_time OR class.end_time>$str_start_time)
-    AND class.status!= 4
     AND class.status!= 5
     AND class.status!= 6
     AND (round.teach_status=1 OR round.teach_status=2)
     AND round.sale_status!=0
-    AND round.sale_status!=1";
+    AND round.sale_status!=1
+    AND round.sale_status!=6";
             return $this->db->query($sql)->result_array();
         }
         
