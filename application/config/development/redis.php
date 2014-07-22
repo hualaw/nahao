626983e40a0ca4ba971cf0c87a1da567bdbd7341
shangshikai <shangshikai@tizi.com>
2014-07-22 15:05:43 +0800

    增加找回验证码功能

--- application/config/development/redis.php
+++ application/config/development/redis.php
@@ -7,7 +7,7 @@ $config['redis_db']['order'] 		= 3; //订单
 
 
 // Default connection group
-$config['redis_default']['host'] = '192.168.11.75';     // IP address or host
+$config['redis_default']['host'] = '127.0.0.1';     // IP address or host
 $config['redis_default']['port'] = '6379';      // Default Redis port is 6379
 $config['redis_default']['password'] = '';      // Can be left empty when the server does not require AUTH
 $config['redis_default']['timeout'] = 0.25;
---
+++ application/controllers/admin/identifying.php
@@ -0,0 +1,40 @@
+<?php
+    class Identifying extends NH_Admin_Controller
+    {
+        /**
+         * 查询验证码
+         * @author shangshikai@tizi.com
+         */
+        public function index()
+        {
+            $this->smarty->assign('view','identifying');
+            $this->smarty->display('admin/layout.html');
+        }
+
+        public function dentifying_code()
+        {
+            $phone=trim($this->input->post('phone',TRUE));
+            if(!is_mobile($phone))
+            {
+                redirect('/Identifying');
+            }
+            $arr_captcha=$this->Identifying->code_dentifying($phone);
+            $this->smarty->assign('arr_captcha',$arr_captcha);
+            $this->smarty->assign('phone',$phone);
+            $this->smarty->assign('view','show_phone');
+            $this->smarty->display('admin/layout.html');
+        }
+
+        public function eny_phone()
+        {
+            $phone=trim($this->input->post('phone',TRUE));
+            if(!is_mobile($phone))
+            {
+                echo 0;
+            }
+            else
+            {
+                echo 1;
+            }
+        }
+    }
