3618411909f1aef64c074e1ca7cdfc3df7c0bb15
shanghongliang <shanghongliang@tizi.com>
2014-08-14 16:16:24 +0800

    1.我的课程页面pad切图 100% 2.我的课程页面相关素材 3.我的课程页面的tab切换js

--- static/debug/module/studentHomePage/tab_nav.js
+++ static/debug/module/studentHomePage/tab_nav.js
@@ -5,8 +5,9 @@ define(function(require,exports){
 			var _index=$(this).index(),_link=$(this).attr("link");
 			$(this).parent().children().removeClass("active");
 			$(this).addClass("active");
-			$(".list_tabs .tabContent").children().removeClass("active");
-			$(".list_tabs .tabContent").children(".item[itemname="+_link+"]").addClass("active");
+			var _item=$(this).parents(".list_tabs").find(".tabContent");
+			_item.children().removeClass("active");
+			_item.children(".item[itemname="+_link+"]").addClass("active");
 			e.preventDefault();
 		});
 	}
