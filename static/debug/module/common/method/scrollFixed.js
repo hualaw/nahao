829042d091ca6d1e0ba39d9ddef13d91563d0fcb
shanghongliang <shanghongliang@tizi.com>
2014-08-18 09:15:54 +0800

    1.首页的fixed效果放到了homepage.js中 2.首页在浏览记录里加了一个div 3.首页的验证信息样式调整

--- static/debug/module/common/method/scrollFixed.js
+++ static/debug/module/common/method/scrollFixed.js
@@ -4,25 +4,10 @@
 define(function(require,exports){
 	//fixedinit初始化
 	exports.init=function(){
-		//首页课程列表的fixed
-		exports.fiexed($(".cListWrap .chTitle"));
-		//首页广告区域的fixed
-		exports.fiexed($(".historyWrap .historyListWrap"));
+		
 	},
 	exports.fiexed=function(item,top){
-		if(!top){
-			top=parseInt($(item).parent().prop("offsetTop"));
-		}
-		var _left=parseInt($(item).parent().prop("offsetLeft"));
-		$(window).scroll(function(){
-			var _windowTop=$(window).scrollTop();
-			if(_windowTop>=top){
-				$(item).css({"position":"fixed","top":"0px;","left":_left+"px","z-index":"1111"});
-			}else{
-				$(item).css({"position":"absolute","top":"0px;","left":"0px","z-index":"1"});
-			}
-			var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
-		});
+		
 	}
 
 });
