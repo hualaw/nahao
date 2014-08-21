45c67e0d381dc4a1ac9114d6a7be99ccb6fc9041
songweijun <songweijun@tizi.com>
2014-08-21 11:49:32 +0800

    update

--- application/controllers/www/classes.php
+++ application/controllers/www/classes.php
@@ -48,18 +48,11 @@ class Classes extends NH_User_Controller {
     	$this->smarty->assign('view_list' 		, $view_list);
     	$this->smarty->display('www/student_class/index.html');
     }
-    // 课程列表----学科辅导
-//    function xueke(){
-//    	$this->smarty->display('www/student_class/index.html');
-//    }
-//    // 课程列表----素质教育
-//    function suzhi(){
-//    	$this->smarty->display('www/student_class/index.html');
-//    }
+    
     // 课程详情页面
-    function article(){
-    	$this->smarty->display('www/student_class/article.html');
-    }
+//    function article(){
+//    	$this->smarty->display('www/student_class/article.html');
+//    }
 
 }
 
