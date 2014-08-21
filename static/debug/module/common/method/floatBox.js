6c740fb7e990e80636680121bf35149a1fc26ebf
shanghongliang <shanghongliang@tizi.com>
2014-08-21 14:09:46 +0800

    1.全站清除浏览记录微调

--- static/debug/module/common/method/floatBox.js
+++ static/debug/module/common/method/floatBox.js
@@ -57,7 +57,8 @@ define(function(require,exports){
     	//清空浏览记录
 	    if($(".historyWrap .deletHistory").length){
 	    	$(".historyWrap .deletHistory").click(function(e){
-	    		$('.historyList').remove().append("<div class=\"noHistory\">暂无浏览记录</div>");
+	    		$('.historyList').remove();
+	    		$(".historyWrap .historyListWrap").append("<div class=\"noHistory\">暂无浏览记录</div>");
 	    		//清除cookie
 	    		document.cookie = 'recent_view' + "=; expires=Fri, 31 Dec 1999 23:59:59 GMT;";
 	    		e.preventDefault();
