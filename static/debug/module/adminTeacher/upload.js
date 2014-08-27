353843ce4201e8a7dd0a359b71d8f6dbffcc5fea
shangshikai <shangshikai@tizi.com>
2014-08-27 18:31:02 +0800

    图片修改

--- static/debug/module/adminTeacher/upload.js
+++ static/debug/module/adminTeacher/upload.js
@@ -24,6 +24,7 @@ define(function(require,exports){
 //                console.log(img_url_general);
                 $("#teacher_authimg").val(img_url);
                 $('#img_url_general').attr('src',img_url_general);
+                $('#show_3001').html('300*225');
                 /*添加教师资格证书图片*/
             }
         });
@@ -49,6 +50,7 @@ define(function(require,exports){
 //                console.log(img_url_general);
                 $("#title_authimg").val(img_url);
                 $('#img_title_general').attr('src',img_url_general);
+                $('#show_3002').html('300*225');
                 /*添加教师职称证书图片*/
             }
         });
@@ -76,6 +78,7 @@ define(function(require,exports){
 //               /*添加学校工作证图片*/
                 $("#work_authimg").val(img_url);
                 $("#img_work_general").attr('src',img_url_general);
+                $('#show_3003').html('300*225');
             }
         });
 
@@ -95,12 +98,41 @@ define(function(require,exports){
                     'general_width' : teacher_title_width,
                     'general_height' : teacher_title_height
                 };
+                var sizehundred = {
+                    'general_width' : 100,
+                    'general_height' : 100
+                };
+                var sizeseventy = {
+                    'general_width' : 70,
+                    'general_height' : 70
+                };
+                var sizefifty = {
+                    'general_width' : 50,
+                    'general_height' : 50
+                };
+                var sizefortyfive = {
+                    'general_width' : 45,
+                    'general_height' : 45
+                };
                 var img_url = data.key;
                 var img_url_general = _qiniu_url+data.key+'?imageView/2/w/'+size.general_width+'/h/'+size.general_height;
+                var img_urlhundred_general = _qiniu_url+data.key+'?imageView/2/w/'+sizehundred.general_width+'/h/'+sizehundred.general_height;
+                var img_urlseventy_general = _qiniu_url+data.key+'?imageView/2/w/'+sizeseventy.general_width+'/h/'+sizeseventy.general_height;
+                var img_urlfifty_general = _qiniu_url+data.key+'?imageView/2/w/'+sizefifty.general_width+'/h/'+sizefifty.general_height;
+                var img_urlfortyfive_general = _qiniu_url+data.key+'?imageView/2/w/'+sizefortyfive.general_width+'/h/'+sizefortyfive.general_height;
 //                console.log(img_url);
 //                console.log(img_url_general);
+                $('#show_130').html('130*130');
+                $('#show_100').html('100*100');
+                $('#show_70').html('70*70');
+                $('#show_50').html('50*50');
+                $('#show_45').html('45*45');
                 $("#teacher_avatar_img").val(img_url);
                 $('#img_avatar').attr('src',img_url_general);
+                $('#img_urlhundred_general').attr('src',img_urlhundred_general);
+                $('#img_urlseventy_general').attr('src',img_urlseventy_general);
+                $('#img_urlfifty_general').attr('src',img_urlfifty_general);
+                $('#img_urlfortyfive_general').attr('src',img_urlfortyfive_general);
                 /*添加教师职称证书图片*/
             }
         });
