f7b962f2e27345d721f5f01c0c0894a202402998
shangshikai <shangshikai@tizi.com>
2014-09-01 13:04:09 +0800

    //

--- static/debug/module/adminOrder/order.js
+++ static/debug/module/adminOrder/order.js
@@ -149,7 +149,7 @@ define(function(require,exports){
                 url:"/order/modify_price",
                 data:"modify_price="+$("#price_modify").val()+"&order_id="+$('#student_order_id').html()+"&spend="+$('#spend').html(),
                 success:function(msg){
-                    console.log(msg);
+                    //alert(msg);
                     if(msg==1)
                     {
                         location.reload();
