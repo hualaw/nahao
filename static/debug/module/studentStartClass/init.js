40ec914a3e331e3c652c5da47670ab0128c6f886
liuhua <liuhua@tizi.com>
2014-09-04 19:02:18 +0800

    修复我要开课试讲时间出不来的bug

--- static/debug/module/studentStartClass/init.js
+++ static/debug/module/studentStartClass/init.js
@@ -11,17 +11,19 @@ define(function(require,exports){
 	//判断当前页面时注册成功的关于我的页面
 	if($('.writeInfo').length > 0){
 		// 美化select
-		//$('select').jqTransSelect();
-		// 美化radio
-		$('input[type=radio]').jqTransRadio();
-		// 美化checkBox
-		$('input[type=checkbox]').jqTransCheckBox();
-
 		// require("module/studentStartClass/edit").edit();
 	    //时间插件
 	    require("module/studentStartClass/datePlugin").addDatePlugin();
    		//我要开课 试讲 信息 验证
 		_valid.writeInfoForm();
+
+        require.async('select', function(ex){
+            $('select').jqTransSelect();
+            // 美化radio
+            $('input[type=radio]').jqTransRadio();
+            // 美化checkBox
+            $('input[type=checkbox]').jqTransCheckBox();
+        });
 	}
 	if($(".regTeacher").length){
     	//我要开课 老师注册验证
