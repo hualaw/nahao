92118d1cb4b6c0d6d1702392c249db89f5a0b9dc
liuhua <liuhua@tizi.com>
2014-09-05 08:58:59 +0800

    remove function() in init.js

--- static/debug/module/studentStartClass/init.js
+++ static/debug/module/studentStartClass/init.js
@@ -1,37 +1,29 @@
-define(function(require,exports){	
-
-	if ($(".popBox").length>0) {
-		require("module/common/method/popUp").popUp(".popBox");
-	}
-
-	//判断当前页面时注册成功的关于我的页面
-	if($('.writeInfo').length > 0){
+define(function(require,exports){
         // 公共select模拟
         require('select');
-
+        if ($(".popBox").length>0) {
+            require("module/common/method/popUp").popUp(".popBox");
+        };
         require("module/common/method/setSchool");
-
-        //时间插件
-        require("module/studentStartClass/datePlugin").addDatePlugin();
-
-        //我要开课 试讲 信息 验证
+        //我要开课验证
         var _valid = require("module/studentStartClass/valid");
-        _valid.writeInfoForm();
-
-        require.async("select", function(ex) {
+        //判断当前页面时注册成功的关于我的页面
+        if($('.writeInfo').length > 0){
             // 美化select
             $('select').jqTransSelect();
             // 美化radio
             $('input[type=radio]').jqTransRadio();
-            // 美化checkBox
+            // 美化checkBo
             $('input[type=checkbox]').jqTransCheckBox();
 
-            //require("module/studentStartClass/edit").edit();
-        });
-    }
-
-	if($(".regTeacher").length){
-    	//我要开课 老师注册验证
-		_valid.teaRegForm();
-	}
+            // require("module/studentStartClass/edit").edit();
+            //时间插件
+            require("module/studentStartClass/datePlugin").addDatePlugin();
+            //我要开课 试讲 信息 验证
+            _valid.writeInfoForm();
+        }
+        if($(".regTeacher").length){
+            //我要开课 老师注册验证
+            _valid.teaRegForm();
+        }
 })
