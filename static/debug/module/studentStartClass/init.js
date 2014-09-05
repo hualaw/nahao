3e6411326f4c9c4bc90080c16748c0628e75eeb4
liuhua <liuhua@tizi.com>
2014-09-05 08:48:27 +0800

    fix datepicker bug

--- static/debug/module/studentStartClass/init.js
+++ static/debug/module/studentStartClass/init.js
@@ -1,10 +1,8 @@
 define(function(require,exports){	
-	$(function (){
+
 	if ($(".popBox").length>0) {
 		require("module/common/method/popUp").popUp(".popBox");
-	};
-
-	//我要开课验证
+	}
 
 	//判断当前页面时注册成功的关于我的页面
 	if($('.writeInfo').length > 0){
@@ -19,7 +17,7 @@ define(function(require,exports){
         //我要开课 试讲 信息 验证
         var _valid = require("module/studentStartClass/valid");
         _valid.writeInfoForm();
-        /*
+
         require.async("select", function(ex) {
             // 美化select
             $('select').jqTransSelect();
@@ -28,14 +26,12 @@ define(function(require,exports){
             // 美化checkBox
             $('input[type=checkbox]').jqTransCheckBox();
 
-            require("module/studentStartClass/edit").edit();
+            //require("module/studentStartClass/edit").edit();
         });
-        */
     }
 
 	if($(".regTeacher").length){
     	//我要开课 老师注册验证
 		_valid.teaRegForm();
 	}
-	})
 })
